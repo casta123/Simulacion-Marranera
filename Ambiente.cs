18aeee2193e3a8576b4f8f0e5d721c32ca69d7c6
casta123 <andrescasta24@gmail.com>
2016-05-18 23:31:25 -0500

    Update Ambiente.cs

--- Ambiente.cs
+++ Ambiente.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Ambiente : MonoBehaviour {
@@ -6,13 +7,36 @@ public class Ambiente : MonoBehaviour {
 	public GameObject marrano;
 	public float Temperatura; 
 	public int dia;
+	public Ambiente instance = null;
+	public Text Descripcion;
+
+
 	// Use this for initialization
-	void Start () {
-		dia = 0;
+	void Awake () {
+		if (instance == null)
+			instance = this;
+		else
+			Destroy (gameObject);
+		DontDestroyOnLoad (gameObject);
+		dia = 1;
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void Update(){
+		if(Input.GetKeyDown(KeyCode.Space))
+			StartCoroutine (Simular());
 	}
+
+	IEnumerator Simular(){
+		if (dia < 90) {
+			var m =marrano.GetComponent<Marrano> ();
+			m.Alimentar ();
+			Descripcion.text ="Dia: " + dia + " Peso: " + m.Peso.ToString("N2") + "Kg Etapa: " +  m.Etapa.nombre;
+			dia += 1;
+		}
+		yield return new WaitForSeconds (3);
+	}
+
+
+
 }
