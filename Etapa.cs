da58526ce3243d2eea45ba509f57b9e40bad28d9
casta123 <andrescasta24@gmail.com>
2016-05-18 23:32:03 -0500

    Add files via upload

---
+++ Etapa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Etapa
+{
+	public string nombre;
+	public int id;
+	public Etapa (int id,string nombre)
+	{
+		this.id = id;
+		this.nombre = nombre;
+	}
+
+
+}
+
+
---
+++ EtapaMarrano.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class EtapaMarrano
+{
+	public Etapa LACTANCIA = new Etapa(0,"Lactancia");
+	public Etapa DESTETE = new Etapa(1,"Destete");
+	public Etapa DESARROLLO = new Etapa(2,"Desarrollo");
+	public Etapa ENGORDE = new Etapa(3,"Engorde");
+
+}
+
